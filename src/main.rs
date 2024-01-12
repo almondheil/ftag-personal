05a0d697b88ce8ff3a7fb6f7de4ee42d1b534fa1
almond heil <almond@almendra.dev>
2024-01-11 20:26:37 -0700

    Add rename function to rename a tag

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::{io::ErrorKind, collections::HashSet};
 use camino::Utf8PathBuf;
 use clap::{Parser, Subcommand};
-use itertools::Itertools;
 
 mod ftag;
-use ftag::FtagError;
+use ftag::{FtagError, get_file_tags};
 
 /// Utility to tag files for easy access
 #[derive(Debug, Parser)]
@@ -20,7 +19,7 @@ enum Commands {
     Init,
 
     /// List tags of a path or whole database
-    Tags {
+    List {
         /// Target path
         path: Option<Utf8PathBuf>,
     },
@@ -53,17 +52,29 @@ enum Commands {
         #[arg(required = true)]
         tags: Vec<String>,
     },
+
+    /// Rename a single tag for a path
+    #[command(arg_required_else_help = true)]
+    Rename {
+        /// Target path
+        path: Utf8PathBuf,
+
+        /// Old tag name
+        old_tag: String,
+
+        /// New tag name
+        new_tag: String,
+    }
 }
 
-fn display_tags(name: String, tags: HashSet<String>) {
+fn display_tags(tags: HashSet<String>) {
     // Get the HashSet as a vector and alphabetize it
     let mut tags: Vec<_> = tags.into_iter().collect();
     tags.sort(); // alphabetic and case-sensitive
 
     // Print them out with a little header 
-    println!("{} tags:", name);
     for tag in tags {
-        println!("  {}", tag);
+        println!("{}", tag);
     }
 }
 
@@ -82,20 +93,20 @@ fn main() {
             }
         }
 
-        Commands::Tags { path } => match path {
+        Commands::List { path } => match path {
             Some(path) => {
                 match ftag::get_file_tags(&path) {
                     Err(err) => match err {
                         FtagError::IoError(ErrorKind::NotFound) => eprintln!("Filepath {} does not exist!", path),
                         _ => eprintln!("{}", err.to_string())
                     }
-                    Ok(tags) => display_tags(path.to_string(), tags),
+                    Ok(tags) => display_tags(tags),
                 }
             },
             None => {
                 match ftag::get_global_tags() {
                     Err(err) => eprintln!("{}", err.to_string()),
-                    Ok(tags) => display_tags("all file".to_string(), tags),
+                    Ok(tags) => display_tags(tags),
                 }
             },
         },
@@ -106,7 +117,7 @@ fn main() {
                     FtagError::IoError(ErrorKind::NotFound) => eprintln!("Filepath {} does not exist!", path),
                     _ => eprintln!("{}", err.to_string()),
                 },
-                Ok(new_tags) => display_tags(path.to_string(), new_tags),
+                Ok(new_tags) => display_tags(new_tags),
             }
         },
 
@@ -116,7 +127,7 @@ fn main() {
                     FtagError::IoError(ErrorKind::NotFound) => eprintln!("Filepath {} does not exist!", path),
                     _ => eprintln!("{}", err.to_string()),
                 },
-                Ok(new_tags) => display_tags(path.to_string(), new_tags),
+                Ok(new_tags) => display_tags(new_tags),
             }
         },
 
@@ -128,13 +139,43 @@ fn main() {
                     files.sort_by(|a, b| a.to_lowercase().cmp(&b.to_lowercase()));
 
                     // Print them out with a little header 
-                    println!("Files with tags {}", tags.iter().format(" "));
                     for file in files {
-                        println!("  {}", file);
+                        println!("{}", file);
                     }
                 },
             }
-
         },
+
+        Commands::Rename { path, old_tag, new_tag} => { 
+            // Determine whether the path contains old_tag
+            let current_tags = get_file_tags(&path);
+            match current_tags {
+                Err(err) => match err {
+                    FtagError::IoError(ErrorKind::NotFound) => eprintln!("Filepath {} does not exist!", path),
+                    _ => eprintln!("{}", err.to_string()),
+                },
+                Ok(tags) => {
+                    if !tags.contains(&old_tag) {
+                        eprintln!("Tag {} not found.", old_tag);
+                        return;
+                    }
+
+                    // Remove the old tag and swap in the new one
+                    if let Err(err) = ftag::remove_tags(&path, vec![old_tag]) {
+                        eprintln!("{}", err.to_string());
+                    }
+                    if let Err(err) = ftag::add_tags(&path, vec![new_tag]) {
+                        eprintln!("{}", err.to_string());
+                    }
+
+                    // Print out the properly updated tags
+                    match ftag::get_file_tags(&path) {
+                        Err(err) => eprintln!("{}", err.to_string()),
+                        Ok(tags) => display_tags(tags),
+                    }
+                }
+            }
+
+        }
     }
 }
